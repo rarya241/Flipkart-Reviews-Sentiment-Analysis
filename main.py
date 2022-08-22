10f8e802a41d475517f56afb562387325a22a436
rarya241 <84728131+rarya241@users.noreply.github.com>
2022-08-22 20:43:44 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+
+data = pd.read_csv("")
+print(data.head())
+# Checking for Null Values in the Data.
+print(data.isnull().sum())
+#preparing the data for the sentiment analysis
+import nltk
+import re
+nltk.download('stopwords')
+stemmer = nltk.SnowballStemmer("english")
+from nltk.corpus import stopwords
+import string
+stopword=set(stopwords.words('english'))
+
+def clean(text):
+    text = str(text).lower()
+    text = re.sub('\[.*?\]', '', text)
+    text = re.sub('https?://\S+|www\.\S+', '', text)
+    text = re.sub('<.*?>+', '', text)
+    text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
+    text = re.sub('\n', '', text)
+    text = re.sub('\w*\d\w*', '', text)
+    text = [word for word in text.split(' ') if word not in stopword]
+    text=" ".join(text)
+    text = [stemmer.stem(word) for word in text.split(' ')]
+    text=" ".join(text)
+    return text
+data["Review"] = data["Review"].apply(clean)
+ratings = data["Rating"].value_counts()
+numbers = ratings.index
+quantity = ratings.values
+# Pie Chart for showing different reviews
+import plotly.express as px
+figure = px.pie(data,
+             values=quantity,
+             names=numbers,hole = 0.5)
+figure.show()
+# Analysing Sentiments of the reviews
+nltk.download('vader_lexicon')
+sentiments = SentimentIntensityAnalyzer()
+data["Positive"] = [sentiments.polarity_scores(i)["pos"] for i in data["Review"]]
+data["Negative"] = [sentiments.polarity_scores(i)["neg"] for i in data["Review"]]
+data["Neutral"] = [sentiments.polarity_scores(i)["neu"] for i in data["Review"]]
+data = data[["Review", "Positive", "Negative", "Neutral"]]
+print(data.head())
+
+x = sum(data["Positive"])
+y = sum(data["Negative"])
+z = sum(data["Neutral"])
+#Giving a Sentiment score to the reviews
+def sentiment_score(a, b, c):
+    if (a>b) and (a>c):
+        print("Positive 😊 ")
+    elif (b>a) and (b>c):
+        print("Negative 😠 ")
+    else:
+        print("Neutral 🙂 ")
+sentiment_score(x, y, z)
